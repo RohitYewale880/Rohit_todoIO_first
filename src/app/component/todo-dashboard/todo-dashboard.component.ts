92317b990dbbfc5e82b082172a8162e64e421206
RohitYewale880 <rohityewale2020@gmail.com>
2026-05-27 16:55:02 +0530

    localstorage

--- src/app/component/todo-dashboard/todo-dashboard.component.ts
+++ src/app/component/todo-dashboard/todo-dashboard.component.ts
@@ -15,20 +15,30 @@ export class TodoDashboardComponent implements OnInit {
   ) { }
   tododata !: Array<Itodo>
   getEditedtodo !:Itodo
+
   ngOnInit(): void {
-    this.tododata = todoArr
+    if(localStorage.getItem('todoArr')){
+      this.tododata = JSON.parse(localStorage.getItem('todoArr') || '[]')
+    }
+    else{
+      this.tododata = []
+    }
+    // this.tododata = todoArr
   }
 
   onChecked(todo: Itodo) {
     let getindex = this.tododata.findIndex((ele) => ele.todoId === todo.todoId)
     this.tododata[getindex] = todo
     console.log(this.tododata);
+    localStorage.setItem('todoArr', JSON.stringify(this.tododata))
   }
 
   AddTodo(todo:Itodo){
     this.tododata.push(todo)
 
     this._snakbar.Opensnakbar(`The todo item with id ${todo.todoId} is added successfully!!!`)
+    localStorage.setItem('todoArr', JSON.stringify(this.tododata))
+
   }
 
   onRemovetodo(id:number){
@@ -36,7 +46,7 @@ export class TodoDashboardComponent implements OnInit {
     let item = this.tododata.splice(getindex, 1)
 
     this._snakbar.Opensnakbar(`The todo item with id ${item[0].todoId} is removed successfully!!!`)
-
+    localStorage.setItem('todoArr', JSON.stringify(this.tododata))
   }
 
   geteditobj(todo : Itodo){
@@ -48,6 +58,6 @@ export class TodoDashboardComponent implements OnInit {
     this.tododata[getindex] = todo
 
     this._snakbar.Opensnakbar(`The todo item with id ${todo.todoId} is updated successfully!!!`)
-
+    localStorage.setItem('todoArr', JSON.stringify(this.tododata))
   }
 }
